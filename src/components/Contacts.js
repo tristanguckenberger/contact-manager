49b47143fe1dc7d61c1d6d990b9b404d349af22e
tristanguckenberger <tristan@gcans.com>
2019-01-12 21:15:32 -0500

    Context reducer addition

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,53 +1,22 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../context";
 
 class Contacts extends Component {
-  state = {
-    contacts: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "jdoe@gmail.com",
-        phone: "555-555-5555"
-      },
-      {
-        id: 2,
-        name: "Karen Smith",
-        email: "karenSmith@gmail.com",
-        phone: "333-333-3333"
-      },
-      {
-        id: 3,
-        name: "Jack Walker",
-        email: "jwalk@gmail.com",
-        phone: "222-222-2222"
-      }
-    ]
-  };
-
-  deleteContact = id => {
-    const { contacts } = this.state;
-
-    const newContact = contacts.filter(contact => contact.id !== id);
-
-    this.setState({
-      contacts: newContact
-    });
-  };
-
   render() {
-    const { contacts } = this.state;
-
     return (
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
